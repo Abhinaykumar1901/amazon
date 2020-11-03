53a16cd8c573c02cb570ae7da4ef6d652cb48f4d
Abhinay Kumar Singh <43674399+Abhinaykumar1901@users.noreply.github.com>
2020-11-03 21:53:26 +0530

    .

--- Header.js
+++ Header.js
@@ -7,12 +7,7 @@ import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
 function Header() {
-  const [{ basket, user }, dispatch] = useStateValue();
-  const handleAuthentication = () => {
-    if (user) {
-      auth.signOut();
-    }
-  }
+  
   return (
     <div className="header">
       <Link to="/"> 
